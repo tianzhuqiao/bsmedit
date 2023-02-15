2b57133e69f3b6c62da93e40fa8354339ff31d0d
Tianzhu Qiao <tq@feiyilin.com>
2023-02-15 09:22:19 -0800

    Save/restore tuple in config.
    Otherwise, json will convert it to a list.

--- bsmedit/mainframe.py
+++ bsmedit/mainframe.py
@@ -84,6 +84,25 @@ class TaskBarIcon(wx.adv.TaskBarIcon):
     def OnTaskBarClose(self, evt):
         wx.CallAfter(self.frame.Close)
 
+class MultiDimensionalArrayEncoder(json.JSONEncoder):
+    def encode(self, obj):
+        def hint_tuples(item):
+            if isinstance(item, tuple):
+                return {'__tuple__': True, 'items': item}
+            if isinstance(item, list):
+                return [hint_tuples(e) for e in item]
+            if isinstance(item, dict):
+                return {key: hint_tuples(value) for key, value in item.items()}
+            else:
+                return item
+
+        return super(MultiDimensionalArrayEncoder, self).encode(hint_tuples(obj))
+
+def hinted_tuple_hook(obj):
+    if '__tuple__' in obj:
+        return tuple(obj['items'])
+    else:
+        return obj
 
 class MainFrame(FramePlus):
 
@@ -260,15 +279,27 @@ class MainFrame(FramePlus):
                 continue
             if not isinstance(value, str):
                 # add sign to indicate that the value needs to be deserialize
-                value = '__bsm__' + json.dumps(value)
+                enc = MultiDimensionalArrayEncoder()
+                value = '__bsm__' + enc.encode(value)
             self.config.SetPath(group)
             self.config.Write(key, value)
 
-    def GetConfig(self, group, key):
+    def GetConfig(self, group, key=None):
         if not group.startswith('/'):
             group = '/' + group
         if self.config.HasGroup(group):
             self.config.SetPath(group)
+            if key is None:
+                rst = {}
+                more, k, index = self.config.GetFirstEntry()
+                while more:
+                    value = self.config.Read(k)
+                    if value.startswith('__bsm__'):
+                        value = json.loads(value[7:], object_hook=hinted_tuple_hook)
+                    rst[k] = value
+                    more, k, index = self.config.GetNextEntry(index)
+                return rst
+
             if self.config.HasEntry(key):
                 value = self.config.Read(key)
                 if value.startswith('__bsm__'):
