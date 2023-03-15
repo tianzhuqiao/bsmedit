047803df887348691b32357edd7ca8660442d0d1
Tianzhu Qiao <tq@feiyilin.com>
2023-03-15 15:27:26 -0700

    Update setup configuration.

--- bsmedit/version.py
+++ bsmedit/version.py
@@ -1,1 +1,10 @@
-__version__ = '3.2.8'
+PROJECT_NAME     = 'bsmedit'
+
+VER_MAJOR        = 3
+VER_MINOR        = 2
+VER_RELEASE      = 9
+
+VER_FLAGS        = "a1"
+
+__version__ = '3.2.9'
+__version__ = f'{VER_MAJOR}.{VER_MINOR}.{VER_RELEASE}{VER_FLAGS}'
--- setup.py
+++ setup.py
@@ -1,25 +1,4 @@
-from setuptools import setup, find_packages
-exec(open("bsmedit/version.py").read())
+from setuptools import setup
 
-long_description = """
-**bsmedit** is a C/C++/SystemC Visualizer.
-- [Documentation](http://bsmedit.feiyilin.com)
-"""
-setup(python_requires='>=3.0',
-      name='bsmedit',
-      version=__version__,
-      description='C/C++/SystemC Visualizer',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      author='Tianzhu Qiao',
-      author_email='tq@feiyilin.com',
-      url='http://bsmedit.feiyilin.com',
-      license="MIT",
-      platforms=["any"],
-      packages=find_packages(),
-      include_package_data=True,
-      entry_points={'gui_scripts': ['bsmedit = bsmedit.__main__:main']},
-      install_requires=[
-          'wxpython>=4.2.0', 'matplotlib', 'numpy', 'click', 'PyOpenGL',
-          'PyOpenGL_accelerate'
-      ])
+if __name__ == "__main__":
+    setup()
