67a8d51a7d98ec621bc57db2dc029a88db474997
Tianzhu Qiao <tq@feiyilin.com>
2024-01-02 19:39:54 -0800

    Remove SystemC from this repo.
    As it will be moved to its own repo.

--- systemc-2.3.1/src/sysc/bsm/bsm_sysc.h
+++
@@ -1,6 +0,0 @@
-#ifndef _BSM_SYSC_H_
-#define _BSM_SYSC_H_
-#include "sysc/bsm/sc_bsm_trace.h"
-#include "sysc/bsm/sc_bsm_trace_buf.h"
-#include "sysc/bsm/bsm_sim_context_impl.h"
-#endif //!defined(_BSM_SYSC_H_)
