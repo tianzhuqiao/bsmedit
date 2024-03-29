4e4b484f9576dfc661c828eff59b676637722ea0
Tianzhu Qiao <tq@feiyilin.com>
2024-03-28 21:49:30 -0700

    ver 3.2.11

--- bsmedit/version.py
+++ bsmedit/version.py
@@ -4,6 +4,6 @@ VER_MAJOR        = 3
 VER_MINOR        = 2
 VER_RELEASE      = 11
 
-VER_FLAGS        = "a1"
+VER_FLAGS        = ""
 
 __version__ = f'{VER_MAJOR}.{VER_MINOR}.{VER_RELEASE}{VER_FLAGS}'
