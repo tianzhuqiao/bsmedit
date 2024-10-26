a10aa13ead35038e7b57de089e11fbc2be59ba65
Tianzhu Qiao <tq@feiyilin.com>
2024-10-25 21:54:09 -0700

    Add --init option to create desktop shortcut.

--- bsmedit/__main__.py
+++ bsmedit/__main__.py
@@ -1,9 +1,11 @@
+import os
 import multiprocessing
 import ctypes
 import wx
 import click
+from bsmutility.utility import create_shortcut
 from .mainframe import MainFrame
-from .version import __version__
+from .version import __version__, PROJECT_NAME
 try:
     ctypes.windll.shcore.SetProcessDpiAwareness(True)
 except:
@@ -49,17 +51,28 @@ class RunApp(wx.App):
               is_flag=True,
               help='Run in debug mode.')
 @click.option('--external/--no-external', default=True,
-              help="Load external modules from bsmplot")
+              help="Load external modules from bsmplot.")
+@click.option('--init', is_flag=True, help=f"Initialize {PROJECT_NAME}, e.g, create desktop shortcut.")
 @click.argument('module', nargs=-1)
-def main(config, path, ignore_perspective, spawn, debug, module, external):
+def main(config, path, ignore_perspective, spawn, debug, module, external, init):
     if spawn and hasattr(multiprocessing, 'set_start_method'):
         multiprocessing.set_start_method('spawn')
+
+    if init:
+        folder, _ = os.path.split(__file__)
+        icns = f'{folder}/ico/mainframe.icns'
+        ico = f'{folder}/ico/mainframe.ico'
+        svg = f'{folder}/ico/mainframe.svg'
+        create_shortcut(PROJECT_NAME, icns, ico, svg)
+        return
+
     app = RunApp(config=config,
                  ignore_perspective=ignore_perspective,
                  path=path,
                  module=module,
                  debug=debug,
                  external=external)
+
     app.MainLoop()
 
 
